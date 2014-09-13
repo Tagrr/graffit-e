d0e0c3005960641521ef03b8c82bab92837674ac
danette59 <59dianeramon@gmail.com>
2014-09-13 11:14:33 +0200

    added nav to layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
 
-  get 'pages/createwall'
+  get "createwall" =>'pages#createwall'
 
-  get 'pages/history'
+  get "history" =>'pages#history'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
